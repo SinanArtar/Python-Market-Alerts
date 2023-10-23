fb998ee61f24484be4d5a73de29780a2ae2b666e
SinanArtar <artargil@gmail.com>
2023-10-23 21:37:40 +0100

    Initial Commit

---
+++ Python Market Alerts/Python_Market_AlertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Python_Market_AlertsApp.swift
+//  Python Market Alerts
+//
+//  Created by Sinan Artar on 23/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Python_Market_AlertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
